eb4104f4818b1b158ef9be36a6885348db239d3e
Jules Wolff Walk <experimentwindows@gmail.com>
2025-10-10 14:37:32 +0200

    (feat) Positionnement des éléments sur l'interface de jeu et rattachement de scripts

---
+++ auditclicker/scripts/car.gd
@@ -0,0 +1,8 @@
+extends Sprite2D
+
+func _on_car_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
+	if event is InputEventMouseButton and event.pressed:
+		print("Sprite cliqué !")
+		position.y += 10
+		await get_tree().create_timer(0.1).timeout
+		position.y -= 10
