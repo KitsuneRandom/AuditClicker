9abd6b5f173a55ba4f5917df077bcb2de212ee28
Jules Wolff Walk <experimentwindows@gmail.com>
2025-10-10 14:37:32 +0200

    (feat) Positionnement des éléments sur l'interface de jeu et rattachement de scripts

---
+++ auditclicker/scripts/upgrades.gd
@@ -0,0 +1,8 @@
+extends Area2D
+
+func _on_paper_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
+	if event is InputEventMouseButton and event.pressed:
+		print("Dossier cliqué !")
+		position.y += 10
+		await get_tree().create_timer(0.1).timeout
+		position.y -= 10
