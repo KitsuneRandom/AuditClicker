a85b8d92dc37ab391ecd54c5ebd33035019f3be5
Jules Wolff Walk <experimentwindows@gmail.com>
2025-10-16 18:00:18 +0200

    (refact) Modfication de l'affichage des stats en jeu

--- auditclicker/scripts/the_game.gd
+++ auditclicker/scripts/the_game.gd
@@ -1,10 +1,13 @@
 extends Node2D
 
-var clicks
+var clicks = 0
+var ppc = 0
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	print("Ready")
 	clicks = 0
+	ppc = 0
+	_updatescoredisplay()
 	pass # Replace with function body.
 
 
@@ -17,6 +20,11 @@ func _getClicks() -> int:
 
 func _increaseClicks() -> void:
 	clicks += 1
+	_updatescoredisplay()
 
 func _printClicks() -> String:
 	return str(clicks)
+
+
+func _updatescoredisplay():
+	$Label.text = "📄 " + str(clicks) + " 📈 " + str(ppc) + " ppc"
