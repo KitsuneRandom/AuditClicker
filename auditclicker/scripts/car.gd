35744ded8c21088955aca8b318b2900fc020cb08
Jules Wolff Walk <experimentwindows@gmail.com>
2025-10-18 19:45:58 +0200

    (feat) Ajout d'un élément essentiel au gameplay

--- auditclicker/scripts/car.gd
+++ auditclicker/scripts/car.gd
@@ -5,6 +5,7 @@ extends Sprite2D
 func _on_car_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
 	if event is InputEventMouseButton and event.pressed:
 		print("Car click !")
+		$"../CarSound".play()
 		position.y += 10
 		await get_tree().create_timer(0.1).timeout
 		position.y -= 10
