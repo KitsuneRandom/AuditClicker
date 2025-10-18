594fc47a35758faa3c153457ab4136a07501ba32
Jules Wolff Walk <experimentwindows@gmail.com>
2025-10-18 23:18:41 +0200

    (feat) Ajout d'un menu au lancement du jeu, d'un compte à rebours pour le temps de jeu et d'un affichage du score à la fin de la partie

--- auditclicker/scripts/paper.gd
+++ auditclicker/scripts/paper.gd
@@ -8,8 +8,8 @@ func _ready() -> void:
 
 func _on_paper_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
 	if event is InputEventMouseButton and event.pressed:
-		print("Paper click ! (" + main._printClicks() + " clicks et " + main._printPpc() + " ppc)")
+		print("Paper click ! (" + main._printpapers() + " papers et " + main._printPpc() + " ppc)")
 		position.y += 10
 		await get_tree().create_timer(0.1).timeout
 		position.y -= 10
-		main._increaseClicks()
+		main._increasepapers()
