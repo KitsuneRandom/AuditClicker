f3855b7b30a9b431af23e53cbb638519cbe084bf
Mathis <mathisdeazevedo26@gmail.com>
2025-11-15 15:30:38 +0100

    (feat) Ajout du menu paramètre

--- auditclicker/scripts/key.gd
+++ auditclicker/scripts/key.gd
@@ -1,8 +1,15 @@
 extends AnimatedSprite2D
 
+var main
+
+func _ready() -> void:
+	main = get_parent().get_parent()
+	pass # Replace with function body.
+
 func _on_key_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
 	if event is InputEventMouseButton and event.pressed:
 		print("Key click !")
 		position.y += 10
 		await get_tree().create_timer(0.1).timeout
 		position.y -= 10
+		main.add_child(preload("res://scenes/settings.tscn").instantiate())
