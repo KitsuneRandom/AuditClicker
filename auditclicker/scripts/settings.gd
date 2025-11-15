79fa28a7d83df932a24fb739375acfa1bb4844b6
Mathis <mathisdeazevedo26@gmail.com>
2025-11-15 16:18:20 +0100

    (feat) Ajout du sous-menu Audio

--- auditclicker/scripts/settings.gd
+++ auditclicker/scripts/settings.gd
@@ -1,7 +1,13 @@
 extends Node2D
 
+var main
+
 func _ready() -> void:
+	main = get_parent().get_parent()
 	pass # Replace with function body.
 
 func _on_opt_button_back_pressed() -> void:
 	queue_free()
+
+func _on_opt_button_audio_pressed() -> void:
+	main.add_child(preload("res://scenes/submenus/settings_audio.tscn").instantiate())
