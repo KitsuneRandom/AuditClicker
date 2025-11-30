f0f5a3ce8d7764797bece6de2a84149f75f65b52
Mathis <mathisdeazevedo26@gmail.com>
2025-11-30 13:53:32 +0100

    (fix) Amélioration de la meilleure feature du jeu

--- auditclicker/scripts/cat.gd
+++ auditclicker/scripts/cat.gd
@@ -2,58 +2,75 @@ extends AnimatedSprite2D
 
 var tooltip
 var main
-var chance
+var clicks
 var buffed
 
 func _ready() -> void:
 	main = get_parent().get_parent()
 	buffed = false
-	chance = 1
+	clicks = 1
 	pass # Replace with function body.
 
 func _on_cat_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
+	if (is_playing()):
+		return
 	if event is InputEventMouseButton and event.pressed:
-		print("Cat click ! (chance = " + str(chance) + ")")
+		print("Cat click ! (clicks = " + str(clicks) + ")")
 		position.y += 10
 		await get_tree().create_timer(0.1).timeout
 		position.y -= 10
 		_trybuff()
-		chance += 1
+		clicks += 1
 
 func _trybuff() -> void:
-	if (chance >= 100):
+	if (clicks > 30):
 		print("Le chat en a marre")
 		queue_free()
-	randomize()
-	var rand = randi_range(1, 100)
-	if (rand > chance):
 		return
-	if (chance >= 50):
+	if (clicks >= 15):
 		_oiiaRemix()
+		if (!buffed):
+			_buff()
 	else:
 		_oiia()
-	if (!buffed):
-		_buff()
+
 
 func _oiia() -> void:
 	print("oiia oiia")
 	play("oiia")
 	$"../Oiia".play()
+	$".".modulate = Color(1, 1, 1)
 	await get_tree().create_timer(2).timeout
+	play("normal")
 	stop()
 
 func _oiiaRemix() -> void:
 	print("oiia oiia")
-	play("oiia", 0.50)
+	play("oiia")
 	$"../OiiaRemix".play()
-	await get_tree().create_timer(2).timeout
+	$".".modulate = Color(0, 1, 0)
+	await get_tree().create_timer(0.5).timeout
+	$".".modulate = Color(1, 1, 0)
+	await get_tree().create_timer(0.5).timeout
+	$".".modulate = Color(1, 0, 0)
+	await get_tree().create_timer(0.5).timeout
+	$".".modulate = Color(1, 0, 1)
+	await get_tree().create_timer(0.5).timeout
+	$".".modulate = Color(0, 1, 1)
+	await get_tree().create_timer(0.5).timeout
+	$".".modulate = Color(0, 0, 1)
+	await get_tree().create_timer(0.5).timeout
+	$".".modulate = Color(1, 1, 1)
+	play("normal")
 	stop()
 
 func _buff() -> void:
-	pass
+	buffed = true
 
 # Surbrillance des éléments
 func _on_cat_mouse_entered() -> void:
+	if (is_playing()):
+		return
 	$".".modulate = Color(0.5, 0.7, 1)
 	tooltip = preload("res://scenes/ui/tooltip.tscn").instantiate()
 	tooltip.displayed_text = "Chat :D"
@@ -61,6 +78,11 @@ func _on_cat_mouse_entered() -> void:
 	main.add_child(tooltip)
 
 func _on_cat_mouse_exited() -> void:
+	if (is_playing()):
+		if tooltip:
+			tooltip.queue_free()
+			tooltip = null
+		return
 	$".".modulate = Color(1, 1, 1)
 	if tooltip:
 		tooltip.queue_free()
