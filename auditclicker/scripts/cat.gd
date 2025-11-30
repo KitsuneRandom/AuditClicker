921175863f380b6f8fd3b6289ab982bd81ef7c6c
Mathis <mathisdeazevedo26@gmail.com>
2025-11-30 11:35:07 +0100

    (feat) Oiia oiia

--- auditclicker/scripts/cat.gd
+++ auditclicker/scripts/cat.gd
@@ -2,18 +2,55 @@ extends AnimatedSprite2D
 
 var tooltip
 var main
+var chance
+var buffed
 
 func _ready() -> void:
 	main = get_parent().get_parent()
+	buffed = false
+	chance = 1
 	pass # Replace with function body.
 
 func _on_cat_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
 	if event is InputEventMouseButton and event.pressed:
-		print("Cat click !")
+		print("Cat click ! (chance = " + str(chance) + ")")
 		position.y += 10
 		await get_tree().create_timer(0.1).timeout
 		position.y -= 10
+		_trybuff()
+		chance += 1
 
+func _trybuff() -> void:
+	if (chance >= 100):
+		print("Le chat en a marre")
+		queue_free()
+	randomize()
+	var rand = randi_range(1, 100)
+	if (rand > chance):
+		return
+	if (chance >= 50):
+		_oiiaRemix()
+	else:
+		_oiia()
+	if (!buffed):
+		_buff()
+
+func _oiia() -> void:
+	print("oiia oiia")
+	play("oiia")
+	$"../Oiia".play()
+	await get_tree().create_timer(2).timeout
+	stop()
+
+func _oiiaRemix() -> void:
+	print("oiia oiia")
+	play("oiia", 0.50)
+	$"../OiiaRemix".play()
+	await get_tree().create_timer(2).timeout
+	stop()
+
+func _buff() -> void:
+	pass
 
 # Surbrillance des éléments
 func _on_cat_mouse_entered() -> void:
