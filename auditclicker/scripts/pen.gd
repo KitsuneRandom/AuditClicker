c8ba38e343e63126dbef45d7c6a54bd979ffc2d5
Mathis DE AZEVEDO <m.deazevedo@unistra.fr>
2025-12-05 10:16:20 +0100

    (feat) Implémentation du déplacement du stylo

--- auditclicker/scripts/pen.gd
+++ auditclicker/scripts/pen.gd
@@ -2,10 +2,40 @@ extends AnimatedSprite2D
 
 var tooltip
 var main
+var is_mouse_over
+var isclicked = false
 
 func _ready() -> void:
 	main = get_parent().get_parent()
+	set_process_input(true)
 	pass # Replace with function body.
+
+func _process(delta: float) -> void:
+	if isclicked:
+		_followmouse()
+	else:
+		pass
+
+func _followmouse() -> void:
+	position = main.get_global_mouse_position()
+
+func _fall() -> void:
+	while position.y < 74:
+		position.y += 1
+		await get_tree().create_timer(0.1).timeout
+	if position.y >= 74:
+		for i in range(0, 100):
+			position.y += 1
+			await get_tree().create_timer(0.1).timeout
+		position.x = 57
+		position.y = 74
+
+func _input(event: InputEvent) -> void:
+	if event is InputEventMouseButton:
+		if event.pressed and is_mouse_over:
+			isclicked = true
+		else:
+			isclicked = false
 
 func _on_pen_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
 	if event is InputEventMouseButton and event.pressed:
@@ -20,6 +50,7 @@ func _on_pen_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> v
 
 # Surbrillance des éléments
 func _on_pen_mouse_entered() -> void:
+	is_mouse_over = true
 	$".".modulate = Color(0.5, 0.7, 1)
 	tooltip = preload("res://scenes/ui/tooltip.tscn").instantiate()
 	tooltip.displayed_text = "Stylo"
@@ -27,6 +58,7 @@ func _on_pen_mouse_entered() -> void:
 	main.add_child(tooltip)
 
 func _on_pen_mouse_exited() -> void:
+	is_mouse_over = false
 	$".".modulate = Color(1, 1, 1)
 	if tooltip:
 		tooltip.queue_free()
