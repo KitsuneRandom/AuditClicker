e9b43375163179ed249425b2424a984988f36402
Jules Wolff Walk <experimentwindows@gmail.com>
2025-12-17 17:42:54 +0100

    (feat) Ajout du système d'attente et de progression sur la phase restitution

--- auditclicker/scripts/restitution.gd
+++ auditclicker/scripts/restitution.gd
@@ -1,9 +1,15 @@
 extends Node2D
 
 var main
+var displayed_text
+var countdown
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	main = get_parent()
+	$Valider.visible = false
+	displayed_text = $RestitutionText.text
+	$RestitutionText.text = ""
+	countdown = main._getPhaseStepDuration("restitution")
 	pass # Replace with function body.
 
 
@@ -12,6 +18,12 @@ func _on_valider_pressed() -> void:
 	queue_free()
 
 func _on_rapport_pressed() -> void:
+	$Rapport.visible = false
+	$RestitutionText.visible = true
+	for i in range(displayed_text.length()):
+		$RestitutionText.text = displayed_text.substr(0, i + 1)
+		await get_tree().create_timer(countdown/displayed_text.length()).timeout
+	$Valider.visible = true
 	pass # Replace with function body.
 
 
