59a5ca024452589f29a6a286b977bc5a759f0339
Jules Wolff Walk <experimentwindows@gmail.com>
2025-12-18 10:26:04 +0100

    (feat) Ajout d'une fonction pour mise à jour du texte

--- auditclicker/scripts/tooltip.gd
+++ auditclicker/scripts/tooltip.gd
@@ -1,13 +1,14 @@
 extends Node2D
 
 @export var displayed_text: String
+var font
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	$".".z_index = 100
 	$Background.size = Vector2(10 * 2, 10 * 2)
 	$Background/Label.text = ""
-	var font = $Background/Label.get_theme_font("font")
+	font = $Background/Label.get_theme_font("font")
 	for i in range(displayed_text.length()):
 		$Background/Label.text = displayed_text.substr(0, i + 1)
 		var text_size = font.get_string_size($Background/Label.text)
@@ -24,3 +25,10 @@ func _process(delta: float) -> void:
 	end_pos -= Vector2(0, -30)
 	position = end_pos
 	pass
+
+
+func _set_text(new_text: String) -> void:
+	$Background/Label.text = displayed_text
+	var text_size = font.get_string_size($Background/Label.text)
+	$Background.size.x = text_size.x + 10 * 2
+	$Background.size.y = text_size.y + 10
