4df1c5257cc09900cc8f36bddd92d57470378e8f
Mathis DE AZEVEDO <m.deazevedo@unistra.fr>
2025-12-19 11:04:45 +0100

    (fix) Ajustements sur la phase d'analyse

--- auditclicker/scripts/analyse.gd
+++ auditclicker/scripts/analyse.gd
@@ -18,12 +18,13 @@ var countdown
 
 var texts = [
 	"Pour éviter de \nperdre du temps, il \nest essentiel \nd'identifier les \ntâches à haut \nrendement.",
-	"L'écoute et la \ncommunication permet \nd'avoir des opportunités \nd'innovation.",
+	"L'écoute et la \ncommunication permet \nd'avoir des \nopportunités \nd'innovation.",
 	"Anticiper les \nbesoins, suivre de \nprès les dépenses et \nmaîtriser les recettes.",
 	"Offrir une \nexpérience client \nfluide et personnalisée \npour fidéliser \nles clients.",
 	"Transformer \nl’incertitude en \nune opportunité de \ncroissance.",
 	"Chaque membre \nde l’entreprise \ndoit connaître ses \nobjectifs.",
-	"Le crâne d'Ali \nil brille. Son \ncrâne il reflète, \nc'est un miroir"
+	"Le crâne d'Ali \nil brille. Son \ncrâne il reflète, \nc'est un miroir",
+	"Lorem ipsum \ndolor sit amet, \nconsectetur adipiscing \nelit, sed do eiusmod \ntempor"
 ]
 
 ## Fonction appelée à l'instanciation de la scène
@@ -43,21 +44,21 @@ func _ready() -> void:
 func _on_valider_pressed() -> void:
 	if state == 1:
 		$Valider.visible = false
-		displayed_text = texts[randi() % 6]
+		displayed_text = texts[randi() % 7]
 		await _look_with_glass(-38, -56)
-		displayed_text = texts[randi() % 6]
+		displayed_text = texts[randi() % 7]
 		await _look_with_glass(82, -56)
-		displayed_text = texts[randi() % 6]
+		displayed_text = texts[randi() % 7]
 		await _look_with_glass(82, 45)
-		displayed_text = texts[randi() % 6]
+		displayed_text = texts[randi() % 7]
 		await _look_with_glass(-38, 45)
 		$Valider.text = "Valider"
-		$Valider.visible = true
 		$MagnifyingGlass/PaperText.text = ""
 		var target = Vector2(16, -12)
 		var tween = create_tween()
 		tween.tween_property($MagnifyingGlass, "position", target, 1.0) 
 		await tween.finished
+		$Valider.visible = true
 	if state == 2:
 		get_viewport().gui_release_focus()
 		queue_free.call_deferred()
@@ -74,6 +75,7 @@ func _look_with_glass(x: int, y: int) -> void:
 	for i in range(displayed_text.length()):
 		$MagnifyingGlass/PaperText.text = displayed_text.substr(0, i + 1)
 		await get_tree().create_timer(countdown/displayed_text.length()).timeout
+	await get_tree().create_timer(0.5).timeout
 
 ## Changement de la couleur du bouton lorsqu'on passe sur le bouton
 func _on_valider_mouse_entered() -> void:
